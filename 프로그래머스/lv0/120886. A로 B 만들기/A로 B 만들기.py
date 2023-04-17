a29f92a33dd17f6ddf3a78b699cb9eae0511b8fd
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-17 20:36:33 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,5 +1,2 @@
 def solution(before, after):
-    for c in before:
-        if before.count(c) != after.count(c):
-            return 0
-    return 1
+    return 1 if sorted(before)==sorted(after) else 0
