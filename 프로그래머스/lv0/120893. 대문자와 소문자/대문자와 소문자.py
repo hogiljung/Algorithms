c93c621d7e0bfb811d8d4f134a5111772608fe57
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-17 18:25:35 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,8 +1,2 @@
 def solution(my_string):
-    answer = ''
-    for c in my_string:
-        if c == c.upper():
-            answer += c.lower()
-        else:
-            answer += c.upper()
-    return answer
+    return ''.join(c.lower() if c.isupper() else c.upper() for c in my_string)
