f519cd072bbb4830b6a8525c0ad21fb9b741045d
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-19 18:03:07 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,6 +1,2 @@
 def solution(s):
-    s_list = [c for c in s]
-    for c in set(s):
-        s_list.remove(c)
-        
-    return ''.join(sorted(set(s) - set(s_list)))
+    return ''.join(sorted([c for c in set(s) if s.count(c) == 1]))
