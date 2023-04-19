ed2f1e47b3660a17c20db7e53dee63a08209ec7f
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-19 16:33:39 +0900

    [level 0] Title: k의 개수, Time: 9.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,8 +1,2 @@
 def solution(i, j, k):
-    answer = 0
-    for n in range(i, j+1):
-        while n > 0:
-            if n % 10 == k:
-                answer += 1
-            n //= 10
-    return answer
+    return sum([str(n).count(str(k)) for n in range(i, j+1)])
