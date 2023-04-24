f2c4f3d80700af7dc44e9279c697d186c7e2bd6b
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-24 14:47:31 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,17 +1,2 @@
 def solution(sides):
-    answer = 0
-    for i in range(1, max(sides)+1):
-        if isTriangleAvailable(sides, i):
-            answer += 1
-    
-    for i in range(max(sides)+1, sum(sides)):
-        if isTriangleAvailable(sides, i):
-            answer += 1
-    
-    return answer
-
-def isTriangleAvailable(sides, l):
-    sides.append(l)
-    result = max(sides) < sum(sides) - max(sides)
-    sides.pop()
-    return result
+    return 2 * min(sides) - 1
