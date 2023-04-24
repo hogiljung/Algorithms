a503ba3e8ae386074b36d4fa099bae2580d2c826
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-24 16:32:34 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -2,9 +2,22 @@ def solution(spell, dic):
     answer = 2
     
     for word in dic:
-        know = True
+        know = condition = True
+        word_list = list(word)
+        word_set = set(word)
+        for spelling in word_set:
+            word_list.remove(spelling)
+
+        for spelling in word_list:
+            if spelling in word_set:
+                condition = False
+                break
+                
+        if not condition:
+            continue
+        
         for s in spell:
-            if s not in word:
+            if s not in word_set:
                 know = False
                 break
             
