dee41468c45f7b61da1e1656daa8da2b1cc106a3
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-04-24 13:24:43 +0900

    [level 2] Title: 소수 찾기, Time: 0.50 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,25 @@
-from itertools import permutations
+numbers_combinations = set()
 
 def solution(numbers):
-    answer = 0
-    numbers_list = [n for n in numbers]
-    numbers_permutations = []
-    for i in range(1, len(numbers_list)+1):
-        for permutation in permutations(numbers_list, i):
-            numbers_permutations.append(int(''.join(n for n in permutation)))
-            
-    for number in set(numbers_permutations):
-        if isPrime(number):
-            answer += 1
+    makeCombinations("", numbers)
+    
+    answer = len(numbers_combinations)
+    
     return answer
 
+def makeCombinations(str1, str2):
+    print(str1)
+    if str1 != "" and isPrime(int(str1)):
+        numbers_combinations.add(int(str1))
+        
+    for i in range(len(str2)):
+        makeCombinations(str1+str2[i], str2[:i]+str2[i+1:])
+        
 def isPrime(n):
-    if n < 2:
+    if n in (0, 1):
         return False
     
-    for i in range(2, n):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             return False
     return True
