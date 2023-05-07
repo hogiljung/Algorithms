155b0cc6689f5a9e6620f3d9d694ca117270af30
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-07 20:53:43 +0900

    [unrated] Title: 공원 산책, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,7 +1,7 @@
 def solution(park, routes):
     location = [0,0]
-    direction = {'N':[-1,0], 'S':[1,0], 'W':[0,-1], 'E':[0,1]}
-    
+    direction = {'N':(-1,0), 'S':(1,0), 'W':(0,-1), 'E':(0,1)}
+
     for y, row in enumerate(park):
         for x, col in enumerate(row):
             if col == 'S':
@@ -14,7 +14,6 @@ def solution(park, routes):
         for _ in range(int(l)):
             nx += direction[d][1]
             ny += direction[d][0]
-            print(ny, nx)
             if nx>=len(park[0]) or nx<0 or ny>=len(park) or ny<0  or park[ny][nx] == 'X':
                 ny, nx = location
                 break
