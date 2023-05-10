b1220518a7d909ca92a3319701e5cbbee3a3cf31
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-10 16:54:19 +0900

    [unrated] Title: 덧칠하기, Time: 1.82 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,11 +1,10 @@
 def solution(n, m, section):
-    answer = 0
-    i = 0
+    answer = 1
+    prev = section[0]
     
-    while section:
-        start = section.pop()
-        while section and section[-1] > start-m:
-            section.pop()
-        answer += 1
+    for now in section:
+        if now - prev >= m:
+            prev = now
+            answer += 1
         
     return answer
