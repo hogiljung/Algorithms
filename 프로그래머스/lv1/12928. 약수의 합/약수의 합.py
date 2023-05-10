233e13d71b4c81f7f90fd0b432d7b5384c253230
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-10 17:21:28 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -3,9 +3,10 @@ def solution(n):
     
     for i in range(1, int(n**0.5)+1):
         if n % i == 0:
-            answer += i + n // i
+            answer += i + n // i     
             
     if n**0.5 == int(n**0.5):
         answer -= int(n**0.5)
+            
     
     return answer
