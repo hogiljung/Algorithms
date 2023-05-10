85dff241ae8ad1c8b55568977a1e01a6d3c09d37
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-10 19:41:05 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.46 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,10 +1,2 @@
 def solution(arr1, arr2):
-    answer = []
-    
-    for a, b in zip(arr1, arr2):
-        cell = []
-        for x, y in zip(a, b):
-            cell.append(x+y)
-        answer.append(cell)
-        
-    return answer
+    return [[c+d for c, d in zip(a, b)] for a, b in zip(arr1, arr2)]
