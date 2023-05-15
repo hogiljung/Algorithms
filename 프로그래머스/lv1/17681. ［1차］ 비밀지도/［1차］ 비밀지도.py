f5ee0f8c772da3c8c1abb37f8516f1b9448c1ebf
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-16 02:20:58 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,14 +2,6 @@ def solution(n, arr1, arr2):
     answer = []
     
     for a1_row, a2_row in zip(arr1, arr2):
-        a1_row_bin, a2_row_bin = bin(a1_row)[2:].zfill(n), bin(a2_row)[2:].zfill(n)
-        map_row = ''
-        
-        for a1, a2 in zip(a1_row_bin, a2_row_bin):
-            if a1 == '0' and a2 == '0':
-                map_row += ' '
-            else:
-                map_row += '#'
-                
-        answer.append(map_row)
+        map_row = bin(a1_row|a2_row)[2:].zfill(n)
+        answer.append(map_row.replace('1', '#').replace('0', ' '))
     return answer
