0d97d2ebd931454274de972fbe724517124a0125
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-16 06:08:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,19 +1,20 @@
 def solution(s):
-    answer = ''
-    number_word_tb = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
-    i = 0
+    if s.isdigit():
+        return int(s)
     
-    while i < len(s):
-        if s[i].isdigit():
-            answer += str(s[i])
-            i += 1
+    answer = ''
+    number_word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    tmp = ''
+    
+    for c in s:
+        if c.isdigit():
+            answer += str(c)
             continue
-        
-        j = 3
-        while s[i:i+j] not in number_word_tb:
-            j += 1
             
-        answer += number_word_tb[s[i:i+j]]
-        i += j
-        
+        tmp += c
+        if tmp in number_word:
+            answer += str(number_word.index(tmp))
+            tmp = ''
+            
     return int(answer)
