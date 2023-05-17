56b148f22d15cbd315d20a5b15a3e00a00e69d72
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-17 18:25:14 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,10 @@
 def solution(cards1, cards2, goal):
     seperated_goal = [[],[]]
-    
+
     for word in goal:
         if word in cards1:
             seperated_goal[0].append(word)
         else:
             seperated_goal[1].append(word)
-            
-    return 'Yes' if ''.join(seperated_goal[0]) in ''.join(cards1) and ''.join(seperated_goal[1]) in ''.join(cards2) else 'No'
+
+    return 'Yes' if seperated_goal[0] == cards1[:len(seperated_goal[0])] and seperated_goal[1] == cards2[:len(seperated_goal[1])] else 'No'
