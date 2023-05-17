8079571ed689d1734f996319c4b8d2829744d376
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-17 15:59:53 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,2 @@
 def solution(k, m, score):
-    answer = 0
-    score_sorted = sorted(score, reverse=True)
-    
-    for i in range(m-1, len(score_sorted), m):
-        answer += score_sorted[i]*m
-        
-    return answer
+    return sum(sorted(score)[-m::-m])*m
