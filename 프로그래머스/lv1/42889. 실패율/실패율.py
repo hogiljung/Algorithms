0407862aee4fab70b83bc3d170b9cfe99b7d9857
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-17 17:53:20 +0900

    [level 1] Title: 실패율, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,13 +1,15 @@
+from collections import Counter
+
 def solution(N, stages):
+    successed_stage_player_count = Counter(stages)
     fail_rates_stage_tb = {}
     reached_stage_player_count = len(stages)
     
     for stage in range(1, N+1):
         if reached_stage_player_count != 0:
-            player_count_stage = stages.count(stage)
-            fail_rate = player_count_stage / reached_stage_player_count
+            fail_rate = successed_stage_player_count[stage] / reached_stage_player_count
             fail_rates_stage_tb[stage] = fail_rate
-            reached_stage_player_count -= player_count_stage
+            reached_stage_player_count -= successed_stage_player_count[stage]
         else:
             fail_rates_stage_tb[stage] = 0
     
