7ca1fd7daf2d97cd67cef819cd6980bbac38e9ea
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-17 20:05:43 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.51 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,21 @@
+def solution(number, limit, power):
+    answer = 0
+    divisor_counts = [1] + [2] * (number-1)
+    
+    for i in range(2, number+1):
+        tmp = 0
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0:
+                tmp += 1      
+        divisor_counts[i-1] += tmp*2
+        if int(i**0.5) == i**0.5:
+            divisor_counts[i-1] -= 1
+            
+    for divisor_count in divisor_counts:
+        if divisor_count > limit:
+            answer += power
+        else:
+            answer += divisor_count
+    
+    return answer
+
