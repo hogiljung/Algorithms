4d9876c287f2105f9efd0341e49a5db0747bec4f
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-17 14:46:34 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    answer = ''
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    month_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
+    duration_days = b
+    
+    duration_days = sum(month_days[:a]) + b - 1
+    
+    return days[duration_days % 7]
