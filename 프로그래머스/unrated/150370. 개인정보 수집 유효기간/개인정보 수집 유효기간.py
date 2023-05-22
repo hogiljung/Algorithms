eefe7f359de018e6c07128b3a03f2d836a531192
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-22 16:25:42 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,34 +1,20 @@
 def solution(today, terms, privacies):
     answer = []
     terms_dict = {}
-    day_month = 28
     
     for term in terms:
         category, month = term.split()
-        terms_dict[category] = int(month)
+        terms_dict[category] = int(month)*28
     
-    t_year, t_month, t_day = map(int, today.split('.'))
+    today_year, today_month, today_day = map(int, today.split('.'))
+    today = today_year*12*28 + today_month*28 + today_day
     
     for i, p in enumerate(privacies):
         date, category = p.split()
         year, month, day = map(int, date.split('.'))
-        month += terms_dict[category]
-        day -= 1
-        if day == 0:
-            day = day_month
-            month -= 1
-        while month > 12:
-            year += 1
-            month -= 12
-            
-        delete = False
-        if t_year > year:
-            delete = True
-        elif (year==t_year) and (t_month>month):
-            delete = True
-        elif (year==t_year) and (t_month==month) and (t_day>day):
-            delete = True        
-            
-        if delete:
+        entered_day = year*12*28 + month*28 + day
+        expired_day = entered_day + terms_dict[category] - 1
+        if today > expired_day:
             answer.append(i+1)
+        
     return answer
