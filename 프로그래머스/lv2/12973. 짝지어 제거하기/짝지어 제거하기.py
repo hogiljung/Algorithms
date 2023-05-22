a63dcf4905ea4cf886c30f79dd807a3b023cc107
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-05-22 18:48:48 +0900

    [level 2] Title: 짝지어 제거하기, Time: 173.91 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = -1
+    temp = []
+    
+    for c in s:
+        if temp[-1:] == [c]:
+            temp.pop()
+        else:
+            temp.append(c)
+    
+    if temp:
+        return 0
+    else:
+        return 1
