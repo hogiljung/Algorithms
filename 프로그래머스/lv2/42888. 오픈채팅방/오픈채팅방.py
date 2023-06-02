638364a64d127283801c1ea3a4be4699c4954af1
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-02 23:53:54 +0900

    [level 2] Title: 오픈채팅방, Time: 101.63 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,19 +1,14 @@
 def solution(record):
     answer = []
-    nickname_dict = {}
-    result = []
-    do_tb = ['님이 들어왔습니다.', '님이 나갔습니다.']
-    while record:
-        line = record.pop().split()
-        if line[0] == 'Enter' or line[0] == 'Change':
-            if line[1] not in nickname_dict:
-                nickname_dict[line[1]] = line[2]
-        
-        if line[0] != 'Change':
-            do = 0 if line[0] == 'Enter' else 1
-            result.append([line[1], do])
-    
-    while result:
-        uid, do = result.pop()
-        answer.append(nickname_dict[uid] + do_tb[do])
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+    for r in record:
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']:
+            namespace[rr[1]] = rr[2]
+
+    for r in record:
+        if r.split(' ')[0] != 'Change':
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
+
     return answer
