4c9f7813ee0e5396a556baccf8e63d046eb6fef4
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-03 18:00:29 +0900

    [level 2] Title: 땅따먹기, Time: 208.61 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,11 +1,6 @@
 def solution(land):
-    answer = 0
-    temp_land = [[0 for i in range(len(land[0]))] for i in range(len(land))]
-    
-    for i in range(-1, len(land)-1):
-        for j in range(len(land[0])):
-            temp = land[i+1][j] + max(temp_land[i][:j] + temp_land[i][j+1:])
-            if temp > temp_land[i+1][j]: temp_land[i+1][j] = temp
-            
-    answer = max(temp_land[-1])        
-    return answer
+    for i in range(1, len(land)):
+            for j in range(len(land[0])):
+                land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+
+    return max(land[-1])
