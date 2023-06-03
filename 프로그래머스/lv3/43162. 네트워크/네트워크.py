49b455f373f9b73c5824410411a4b7627855848d
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-03 21:53:19 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,20 +1,24 @@
+from collections import deque
+
 def solution(n, computers):
+    global visited
     answer = 0
-    
-    for i in range(len(computers)):
-        if dfs(i, computers):
+    N = len(computers)
+    for i in range(N):
+        if computers[i][i] == 1:
+            bfs(i, computers)
             answer += 1
-    
     return answer
 
-def dfs(v, computers):
-    if computers[v][v] == 0:
-        return False
-    
-    computers[v][v] = 0
-    for i in range(len(computers)):
-        if computers[v][i] == 1:
-            computers[v][i] = 0
-            dfs(i, computers)
-    return True
+def bfs(start, computers):
+    q = deque()
+    q.append(start)
+
+    while q:
+        start = q.popleft()
+        computers[start][start] = 0
+        for end in range(len(computers[start])):
+            if computers[start][end] == 1:
+                computers[start][end] = 0
+                q.append(end)
     
