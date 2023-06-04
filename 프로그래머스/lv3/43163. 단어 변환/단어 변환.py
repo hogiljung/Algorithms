f3bca064a7b43983ac08c844d04a5627254467e8
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-04 14:21:11 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,38 +1,23 @@
-from collections import deque
 def solution(begin, target, words):
-    INF = int(1e9)
-    answer = INF
-    words = [begin] + words
-    graph = [[0 for _ in range(len(words))] for _ in range(len(words))]
-    s = []
-    for i, word in enumerate(words):
-        for j, other_word in enumerate(words):
-            diff = 0
-            for c1, c2 in zip(word, other_word):
-                if i == j:
-                    continue
-                if c1 != c2:
-                    diff += 1
-            if diff == 1:
-                graph[i][j] = 1
-
-    def bfs(v):
-        nonlocal answer
-        q = deque()
-        q.append((v, 0))
-        visited = [False] * len(words)
-        while q:
-            start, count = q.popleft()
-            if words[start] == target:
-                if count < answer:
-                    answer = count
-
-            visited[start] = True
-            for end in range(len(graph)):
-                if graph[start][end] != 0 and not visited[end]:
-                    q.append((end, count+1))
-    bfs(0)
-    return answer if answer != INF else 0
-
+    dist = {begin:0}
+    q = deque([begin])
     
+    while q:
+        current = q.popleft()
+        
+        for next_word in get_adjacent(current, words):
+            if next_word not in dist:
+                dist[next_word] = dist[current]+1
+                q.append(next_word)
     
+    return dist.get(target, 0)
+
+from collections import deque
+
+def get_adjacent(current, words):
+    for word in words:
+        if current == word:
+            continue
+        for i in range(len(current)):
+            if current[:i]+current[i+1:] == word[:i]+word[i+1:]:
+                yield word
