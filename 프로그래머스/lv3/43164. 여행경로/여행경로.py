110177e4c513bf530369e6bc9ce65c556cad8d5d
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-04 17:37:17 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,33 +1,17 @@
 from collections import defaultdict
-answer = []
-END = False
 def solution(tickets):
-    N = len(tickets)
-    used = [False] * N
-    tickets = [(ticket, i) for i, ticket in enumerate(sorted(tickets, key=lambda x: (x[0], x[1])))]
-    dfs('ICN', used, tickets)
-    
-    return answer
+    r = defaultdict(list)
+    for i,j in tickets:
+        r[i].append(j)
+    for i in r.keys():
+        r[i].sort()
 
-def dfs(current, used, tickets):
-    global answer, END
-    answer.append(current)
-    if all(used):
-        END = True
-        return
-    
-    for next_city, ticket_idx in get_adjacent(current, used, tickets):
-        if not used[ticket_idx]:
-            used[ticket_idx] = True
-            dfs(next_city, used, tickets)
-            if not END:
-                answer.pop()
-                used[ticket_idx] = False
-            else:
-                return
-
-def get_adjacent(current, used, tickets):
-    for ticket, i in tickets:
-        if ticket[0] == current and not used[i]:
-            yield ticket[1], i
-            
+    s = ["ICN"]
+    p = []
+    while s:
+        q = s[-1]
+        if r[q] != []:
+            s.append(r[q].pop(0))
+        else:
+            p.append(s.pop())
+    return p[::-1]
