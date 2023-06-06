d063d0d36d758352f3036916e7bfb3b97432d8f3
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-06 18:19:43 +0900

    [level 2] Title: 전화번호 목록, Time: 2419.03 ms, Memory: 376 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,42 @@
 def solution(phone_book):
-    dic = {}
-    for i in phone_book:
-        dic[i] = 0
-        
-    for i in dic:
-        for j in range(len(i)):
-            if (i[:j] in dic):
-                return False
-
+    t = Trie()
+    N = len(phone_book)
+    
+    for i in range(N):
+        if not t.insert(phone_book[i]):
+            return False
+    
+    for i in range(N):
+        if t.search_prefix(phone_book[i]):
+            return False
+    
     return True
+    
+class Trie:
+    class Node:
+        def __init__(self):
+            self.children = {}
+            self.is_end_of_word = False
+    
+    def __init__(self):
+        self.root = self.Node()
+    
+    def insert(self, key):
+        curr_node = self.root
+        for ch in key:
+            if ch not in curr_node.children:
+                curr_node.children[ch] = self.Node()
+            if curr_node.is_end_of_word:
+                return False
+            curr_node = curr_node.children[ch]
+        curr_node.is_end_of_word = True
+        return True
+        
+    def search_prefix(self, key):
+        curr_node = self.root
+        for ch in key:
+            if curr_node.is_end_of_word:
+                return True
+            curr_node = curr_node.children[ch]
+        
+        return False
