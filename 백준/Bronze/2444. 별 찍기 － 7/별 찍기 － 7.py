8b8e696e086a0b4bee49be101e176d365c4efc04
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-15 02:21:27 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,8 +1,4 @@
 N = int(input())
 
 for i in range(-N+1, N):
-    for j in range(abs(i)):
-        print(' ', end='')
-    for j in range(2*(N-abs(i))-1):
-        print('*', end='')
-    print()
+    print(' '*abs(i)+'*'*(2*(N-abs(i))-1))
