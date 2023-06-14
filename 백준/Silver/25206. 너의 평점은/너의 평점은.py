de5800c8a3a1564af79355c9567d36c5bb96dd45
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-06-15 03:46:55 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,13 @@
+import sys
+grade_dict = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0, 'F':0.0}
+total = 0
+total_credit = 0
+
+for s in open(0):
+    _, credit, grade = s.split()
+    if grade == 'P': continue
+
+    credits
+    total += int(credit[0])*grade_dict[grade]
+    total_credit += int(credit[0])
+print(total/total_credit)
