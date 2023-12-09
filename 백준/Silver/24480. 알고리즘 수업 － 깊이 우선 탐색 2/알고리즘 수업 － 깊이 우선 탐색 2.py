05dd26741cd88f5167e667ee12d310b5436b4f11
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-12-09 14:46:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 532 ms, Memory: 60632 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+
+visited = [0] * (N+1)
+graph = [[] for _ in range(N+1)]
+visited_order = 0
+stack = []
+
+def dfs(x):
+    global visited_order
+
+    stack.append(x)
+    while stack:
+        v = stack.pop()
+        if visited[v]:
+            continue
+
+        visited_order += 1
+        visited[v] = visited_order
+
+        stack.extend(graph[v])
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for connecion in graph:
+    connecion.sort()
+
+dfs(R)
+
+print(*visited[1:], sep="\n")
