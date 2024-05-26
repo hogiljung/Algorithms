cdda0a5dfa0201c5bc732938acbf40b67418f82a
hogiljung <112647829+hogiljung@users.noreply.github.com>
2024-05-26 16:43:08 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,20 +1,12 @@
-from functools import cmp_to_key
-
 def solution(numbers):
     # numbers의 모든 요소를 문자열로 변환
     str_numbers = list(map(str, numbers))
     
-    # 커스텀 비교 함수 정의
-    def compare(x, y):
-        return (y + x > x + y) - (y + x < x + y)
+    str_numbers.sort(key=lambda x: (x*3)[:4], reverse=True)
     
-    # 문자열 리스트를 커스텀 비교 함수 기준으로 정렬
-    sorted_numbers = sorted(str_numbers, key=cmp_to_key(compare))
+    largest_number = "".join(str_numbers)
     
-    # 정렬된 문자열 리스트를 합침
-    largest_number = ''.join(sorted_numbers)
-    
-    # 결과가 '0000' 같은 경우를 대비하여 int로 변환 후 다시 문자열로 변환
+    # 결과가 '0000' 같은 경우를 대비
     if largest_number[0] == "0":
         return "0"
     else:
