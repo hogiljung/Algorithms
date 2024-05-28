894426cbf543e2a24d92a2fdcc4fa32e495c0b04
hogiljung <112647829+hogiljung@users.noreply.github.com>
2024-05-28 21:58:57 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,18 @@
 def solution(brown, yellow):
     answer = []
     
-    for i in range(3, int((brown + yellow) ** .5) + 1):
-        if (brown + yellow) % i == 0:
-            r, c = i, (brown + yellow) // i
+    # 두 수의 합이 카펫의 넓이와 같다.
+    area = brown + yellow
+    # 조건에 따라 최소 넓이는 9이고, 최소 세로 길이는 3이다.
+    # 최소 세로 길이인 3부터 시작해서 넓이의 제곱근의 수까지 반복한다.
+    for i in range(3, int(area ** .5) + 1):
+        # 넓이를 나누어 떨어지게 하는 수가 세로 길이가 될 수 있는 후보이다.
+        if area % i == 0:
+            # 나누어 떨어지게 한 수와 그 때의 몫으로 가로 세로 길이를 선언한다.
+            r, c = i, area // i
+            # 가로와 세로에서 각각 2를 뺀 수의 곱이 yellow 의 넓이와 같은 경우를 찾는다.
             if (r - 2) * (c - 2) == yellow:
+                # 그 때의 큰 값이 가로, 작은 값이 세로이다.
                 answer = [max(r, c), min(r, c)]
                 break
                 
