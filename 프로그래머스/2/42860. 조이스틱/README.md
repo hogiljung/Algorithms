088d1b4c9f111be38d432730b0df66d6e7c65667
hogiljung <112647829+hogiljung@users.noreply.github.com>
2024-06-05 00:11:43 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.04 ms
+메모리: 10.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 23:42:06
+2024년 06월 05일 00:11:40
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,42 +1,21 @@
 def solution(name):
     answer = 0
-    need_to_change = []
-    # 모든 문자열을 확인할 때까지 아래를 반복한다.
-    
-    start = 0
-    move = 0
-    
-    
-    
-    
-    for i in range(len(name)):
-        
-    # 양쪽으로 이동하며 변경해야 하는 문자를 찾는다.
-    # 그 때까지의 거리를 더한다.
-    # 위, 아래로의 이동 횟수도 구하여 더한다.
-    
-    
-    # 문자열을 순회하여
-    # 위로 이동하는게 빠른지 아래로 이동하는게 빠른지를 이용해
-    # 각 원소당 이동 횟수를 구할 수 있고,
-    for c, i in enumerate(name):
-        if c == 'A':    
-            continue
-            
-        min_up_or_down_move = min(\
-            ord('A') - ord(c),
-            ord('Z') - ord(c) + 1
-        )
-        answer += min_up_or_down_move
-        
-        min_left_or_right_move = 
-        
-        need_to_change.append((i))
-            
-    
-    # 변경해야 하는 원소의 위치와 이동에 필요한 값을 기록한다.
-    
-    # 현재 가장 가까운 변경해야 하는 원소로 이동하는 값을 더한다.
-    
-    
+    n = len(name)
+
+    def alphabet_to_num(char):
+        num_char = [i for i in range(14)] + [j for j in range(12, 0, -1)]
+        return num_char[ord(char) - ord('A')]
+
+    for ch in name:
+        answer += alphabet_to_num(ch)
+
+    move = n - 1
+    for idx in range(n):
+        next_idx = idx + 1
+        while (next_idx < n) and (name[next_idx] == 'A'):
+            next_idx += 1
+        distance = min(idx, n - next_idx)
+        move = min(move, idx + n - next_idx + distance)
+
+    answer += move
     return answer
