4045d1554e0ee38049ee97989efee724e1070f49
hogiljung <112647829+hogiljung@users.noreply.github.com>
2024-06-10 22:39:40 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,20 +1,23 @@
 def solution(n, times):
     at_most = min(times) * n
     
-    answer = at_most
+    answer = 0
     
     # 심사 시간이 가장 짧은 시간이 모두를 검사하는 시간을 최댓값으로 본다.
     l, r = 1, at_most
 
-    while l <= r:
+    # 이분 탐색으로 최소 시간을 찾는다.
+    while l < r:
         mid = (l + r) // 2
         
+        # 각각 걸리는 시간으로 전체 시간을 나눈 몫을 더하면 검사한 인원 수가 나온다.
         total_checked = sum([mid//time for time in times])
 
         if total_checked < n:
             l = mid + 1
         else:
-            answer = min(answer, mid)
-            r = mid - 1
+            r = mid
+            
+    answer = l
             
     return answer
