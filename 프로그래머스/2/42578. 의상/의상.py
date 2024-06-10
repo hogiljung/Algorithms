d9f71fa1238b500f042bf2bbf5a321e21c73fefb
hogiljung <112647829+hogiljung@users.noreply.github.com>
2024-06-10 23:48:30 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,21 +1,7 @@
+from collections import Counter
+from functools import reduce
+
 def solution(clothes):
-    answer = 0
-    
-    cloth_map = {}
-    
-    for cloth, cloth_type in clothes:
-        if cloth_type not in cloth_map:
-            cloth_map[cloth_type] = 1
-        else:
-            cloth_map[cloth_type] += 1
-        
-    for cloth_count in cloth_map.values():
-        if answer == 0:
-            answer = cloth_count + 1
-        else:
-            answer *= cloth_count + 1
-            
-    if answer > 0:
-        answer -= 1
-    
+    cnt_map = Counter([kind for name, kind in clothes])
+    answer = reduce(lambda x, y: x*(y+1), cnt_map.values(), 1) - 1
     return answer
