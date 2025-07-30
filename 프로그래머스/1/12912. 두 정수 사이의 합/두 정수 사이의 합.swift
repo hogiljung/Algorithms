307503053f75ebea213a95481fb2ecabd334ce46
hogiljung <112647829+hogiljung@users.noreply.github.com>
2025-07-30 20:26:56 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -1,14 +1,4 @@
 func solution(_ a:Int, _ b:Int) -> Int64 {
-    if a == b {
-        return Int64(a)
-    }
-    
-    var count: Int
-    if a < b {
-        count = b-a+1
-    } else {
-        count = a-b+1
-    }
-    
+    var count = a < b ? b-a+1 : a-b+1
     return Int64((a+b) * count / 2)
 }
