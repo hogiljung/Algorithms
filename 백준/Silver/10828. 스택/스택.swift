91faca54bebb656dd96b4ea6d5aeff8f546bb42a
hogiljung <112647829+hogiljung@users.noreply.github.com>
2025-08-03 18:34:48 +0900

    [Silver IV] Title: 스택, Time: 20 ms, Memory: 79652 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.swift
+++ 백준/Silver/10828. 스택/스택.swift
@@ -1,52 +1,22 @@
 import Foundation
 
-struct Stack {
-    private var arr = Array<Int>()
+var s = [Int]()
+let tCase = Int(readLine()!)!
+for _ in 1...tCase {
+    let commandParts = readLine()!.split(separator: " ")
 
-    mutating func push(_ n: Int) -> Void {
-        arr.append(n)
-    }
-
-    mutating func pop() -> Int {
-        let last = arr.popLast()
-        
-        return last == nil ? -1 : last!
-    }
-
-    var top: Int {
-        return empty == 1 ? -1 : arr.last!
-    }
-
-    var empty: Int {
-        return arr.isEmpty ? 1 : 0
-    }
-
-    var size: Int {
-        return arr.count
+    switch commandParts[0] {
+        case "push":
+            s.append(Int(commandParts[1])!)
+        case "pop":
+            s.isEmpty ? print("-1") : print(s.removeLast())
+        case "size":
+            print(s.count)
+        case "empty":
+            s.isEmpty ? print("1") : print("0")
+        case "top":
+            s.last == nil ? print("-1") : print(s.last!)
+        default:
+            continue
     }
 }
-
-func solution() {
-    var s = Stack()
-    let commandCount = Int(readLine()!)!
-
-    for _ in (1...commandCount) {
-        let command = readLine()!.components(separatedBy: " ")
-
-        switch command[0] {
-            case "push":
-                s.push(Int(command[1])!)
-            case "pop":
-                print(s.pop())
-            case "size":
-                print(s.size)
-            case "top":
-                print(s.top)
-            case "empty":
-                print(s.empty)
-            default: continue
-        }
-    }
-}
-
-solution()
