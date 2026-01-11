a9eb06f6671417f0e7ae82442dccd97142d5f26e
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-11 16:32:42 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,11 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
-answer = N
+cnt = N
+
 for _ in range(N):
-    s = input()
-    while s:
-        n = s.count(s[0])
-        if s[0]*n != s[:n]:
-            answer -= 1
+    char_check_tb = set()
+    WORD = input()
+    for curr in WORD:
+        if curr not in char_check_tb:
+            prev = curr
+            char_check_tb.add(curr)
+        elif prev != curr:
+            cnt -= 1
             break
-        s = s[n:]
-print(answer)
+
+print(cnt)
