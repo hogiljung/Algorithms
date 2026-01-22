a7140b75f616db8971178dedad4e6ec97855f21d
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-22 15:43:15 +0900

    [level 2] Title: 할인 행사, Time: 37.96 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -7,19 +7,36 @@ class Solution {
             wMap.put(want[i], number[i]);
         }
         
+        HashMap<String, Integer> dMap = new HashMap<>();
+        for (int i=0; i<10; i++) {
+            String d = discount[i];
+            dMap.put(d, dMap.getOrDefault(d, 0) + 1);
+        }
+        
+        int left = 0;
+        int right = 9;
         int answer = 0;
-        for (int i = 0; i <= discount.length-10; i++) {
-            HashMap<String, Integer> dMap = new HashMap<>();
-            for (int j = i; j < i+10; j++) {
-                String d = discount[j];
-                dMap.put(d, dMap.getOrDefault(d, 0) + 1);
-            }
-            
+        while (right < discount.length) {
             if (wMap.equals(dMap)) {
                 answer++;
             }
+            
+            right++;
+            if (right == discount.length) {
+                break;
+            }
+            String d1 = discount[right];
+            dMap.put(d1, dMap.getOrDefault(d1, 0) + 1);
+            
+            String d2 = discount[left];
+            int cnt = dMap.get(d2);
+            if (cnt == 1) {
+                dMap.remove(d2);
+            } else {
+                dMap.put(d2, cnt-1);
+            }
+            left++;
         }
-        
         return answer;
     }
 }
