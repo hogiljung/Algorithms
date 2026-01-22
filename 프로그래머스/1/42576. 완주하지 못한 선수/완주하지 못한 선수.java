3e8d5c7a96d8873ce690aa8f808cfaca25205ab7
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-22 14:03:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.97 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,26 +4,17 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
         for (String s : completion) {
-            if (!map.containsKey(s)) {
-                map.put(s, 1);
-            } else {
-                map.put(s, map.get(s)+1);
-            }
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         
         String answer = "";
         for (String s : participant) {
-            if (map.containsKey(s)) {
-                int cnt = map.get(s);
-                if (Integer.compare(cnt, 0) == 0) {
-                    answer = s;
-                    break;
-                }
-                map.put(s, cnt-1);
-            } else {
+            int cnt = map.getOrDefault(s, 0);
+            if (Integer.compare(cnt, 0) == 0) {
                 answer = s;
                 break;
             }
+            map.put(s, cnt - 1);
         }
         
         return answer;
