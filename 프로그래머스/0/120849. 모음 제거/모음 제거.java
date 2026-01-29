d859920db7bcc6c884a84bf66ee396497bbd04ad
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-29 16:28:49 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,20 +2,6 @@ import java.util.List;
 
 class Solution {
     public String solution(String my_string) {
-        char[] targets = "aeiou".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        for (char c : my_string.toCharArray()) {
-            boolean isIn = false;
-            for (char t : targets) {
-                if (t == c) {
-                    isIn = true;
-                }
-            }
-            if (isIn) {
-                continue;
-            }
-            sb.append(c);
-        }
-        return sb.toString();
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
