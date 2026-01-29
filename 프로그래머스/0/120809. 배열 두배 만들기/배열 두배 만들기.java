8444b3ae76368149a82c44eadcf9079bbecb0e13
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-29 17:05:58 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,7 +1,9 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        return Arrays.stream(numbers).map(x -> x * 2).toArray();
+        int[] answer = new int[numbers.length];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = numbers[i]*2;
+        }
+        return answer;
     }
 }
