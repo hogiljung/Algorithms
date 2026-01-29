d9658f95e3612e1202e05997dad4e81ef184a51f
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-29 15:52:39 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int n) {
-        double nSqrt = Math.sqrt(n);
-        return Double.compare(Math.ceil(nSqrt), Math.floor(nSqrt)) == 0 ? 1 : 2;
+        return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
