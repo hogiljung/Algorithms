8d1ab2b4974a7d2df50f2d013f0f1260526f4d6b
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-01-29 12:16:51 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.07 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=my_string.length()-1; i>=0; i--) {
-            sb.append(my_string.charAt(i));
-        }
-        
-        return sb.toString();
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.reverse().toString();
     }
 }
