5def949540f2f0dc59d8dbb5b50cb407433c384c
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-04 11:29:37 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.50 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,42 +1,26 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int[] rolls = {a, b, c, d};
-        HashMap<Integer, Integer> rollCnt = new HashMap<>();
-        for (int roll : rolls) {
-            rollCnt.put(roll, rollCnt.getOrDefault(roll, 0) + 1);
-        }
-        
-        ArrayList<Integer> distRolls = new ArrayList<>(rollCnt.keySet());
-        int distRollCnt = rollCnt.size();
+        int[] dices = {a, b, c, d};
+        Arrays.sort(dices);
         int answer = 0;
-        if (distRollCnt == 1) {
-            answer = a * 1111;
-        } else if (distRollCnt == 2) {
-            int cnt = rollCnt.get(distRolls.get(0));
-            if (cnt == 2) {
-                int p = distRolls.get(0);
-                int q = distRolls.get(1);
-                answer = (p + q) * Math.abs(p - q);
-            } else {
-                int p = cnt == 3 ? distRolls.get(0) : distRolls.get(1);
-                int q = cnt == 3 ? distRolls.get(1) : distRolls.get(0);
-                answer = (10 * p + q) * (10 * p + q);
-            }
-        } else if (distRollCnt == 3) {
-            answer = 1;
-            for (int roll : distRolls) {
-                if (rollCnt.get(roll) == 1) {
-                    answer *= roll;
-                }
-            }
-        } else {
-            answer = distRolls.stream()
-                .min(Integer::compareTo)
-                .get();
-        }
         
+        if (dices[0] == dices[3]) {
+            answer = 1111 * dices[3];
+        } else if (dices[0] == dices[2] || dices[1] == dices[3]) {
+            answer = (int) Math.pow(dices[1] * 10 + (dices[0] + dices[3] - dices[1]), 2);
+        } else if (dices[0] == dices[1] && dices[2] == dices[3]) {
+            answer = (dices[0] + dices[2]) * Math.abs(dices[0] - dices[2]);
+        } else if (dices[0] == dices[1]) {
+            answer = dices[2] * dices[3];
+        } else if (dices[1] == dices[2]) {
+            answer = dices[0] * dices[3];
+        } else if (dices[2] == dices[3]) {
+            answer = dices[0] * dices[1];
+        } else {
+            answer = dices[0];
+        }
         return answer;
     }
 }
