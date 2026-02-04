9972de78a71ef719a8f7f94abbfdf3e1e0a4d923
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-04 15:21:52 +0900

    [level 0] Title: 글자 지우기, Time: 0.53 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,17 +1,9 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        boolean[] t = new boolean[my_string.length()];
-        for(int indice : indices) {
-            t[indice] = true;
+        String[] tmp = my_string.split("");
+        for(int i : indices) {
+            tmp[i] = "";
         }
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i<my_string.length(); i++) {
-            if (!t[i]) {
-                sb.append(my_string.charAt(i));
-            }
-        }
-        
-        return sb.toString();
+        return String.join("", tmp);
     }
 }
