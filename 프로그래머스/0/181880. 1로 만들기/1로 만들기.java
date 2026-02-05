ce0d178ec3b0fa0c5f1e7a24870abf5ea10e12c4
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-05 14:44:47 +0900

    [level 0] Title: 1로 만들기, Time: 0.05 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,17 +1,8 @@
 class Solution {
     public int solution(int[] num_list) {
-        int[] t = new int[31];
-        for (int i=2; i<t.length; i++) {
-            if (i % 2 == 0) {
-                t[i] = t[i/2] + 1;
-            } else {
-                t[i] = t[(i-1)/2] + 1;
-            }
-        }
-        
         int answer = 0;
         for (int n : num_list) {
-            answer += t[n];
+            answer += Integer.toBinaryString(n).length() - 1;
         }
         return answer;
     }
