3090c0ce06d10bd072f0fd54e8fba840c7fe704c
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-05 11:07:34 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,31 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
     public String[] solution(String[] str_list) {
-        int l = -1, r = -1;
         for (int i=0; i<str_list.length; i++) {
             String s = str_list[i];
             if (s.equals("l")) {
-                l = i;
-                break;
+                return Arrays.copyOfRange(str_list, 0, i);
             } else if (s.equals("r")) {
-                r = i;
-                break;
+                return Arrays.copyOfRange(str_list, i+1, str_list.length);
             }
         }
-        
-        String[] answer;
-        if (l != -1) {
-            answer = new String[l];
-            for (int i=0; i<l; i++) {
-                answer[i] = str_list[i];
-            }
-        } else if (r != -1) {
-            answer = new String[str_list.length - r - 1];
-            for (int i=0; i<str_list.length - r - 1; i++) {
-                answer[i] = str_list[i+r+1];
-            }
-        } else {
-            answer = new String[0];
-        }
-        return answer;
+        return new String[0];
     }
 }
