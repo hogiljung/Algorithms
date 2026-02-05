f72714b946350580a398ec82262b8bce2a245f4d
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-05 12:10:59 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.33 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr.clone();
+        for (int[] query : queries) {
+            for (int i=query[0]; i<=query[1]; i++) {
+                answer[i]++;
+            }
+        }
+        return answer;
+    }
+}
