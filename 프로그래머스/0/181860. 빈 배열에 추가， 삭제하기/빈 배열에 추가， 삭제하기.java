8c01bcfa028ea960bd60352ef8f2a82fc81239c9
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-09 09:06:09 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.84 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -1,20 +1,29 @@
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        for (int i=0; i<arr.length; i++) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < arr.length; i++) {
             int n = arr[i];
+
             if (flag[i]) {
-                for (int j=0; j<n*2; j++) {
-                    answer.add(n);
+                for (int j = 0; j < n * 2; j++) {
+                    deque.addLast(n);
                 }
             } else {
-                for (int j=0; j<n; j++) {
-                    answer.remove(answer.size()-1);
+                for (int j = 0; j < n; j++) {
+                    deque.pollLast();
                 }
             }
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+
+        int[] answer = new int[deque.size()];
+        int idx = 0;
+        for (int x : deque) {
+            answer[idx++] = x;
+        }
+
+        return answer;
     }
 }
