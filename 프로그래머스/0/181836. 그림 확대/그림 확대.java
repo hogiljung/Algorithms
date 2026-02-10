4fe21720a76fb06117bb4beb12013c158539b61f
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-10 15:00:42 +0900

    [level 0] Title: 그림 확대, Time: 0.61 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -8,9 +8,7 @@ class Solution {
         for (int i=0; i<picture.length; i++) {
             StringBuilder sb = new StringBuilder();
             for (char c : picture[i].toCharArray()) {
-                for (int j=0; j<k; j++) {
-                    sb.append("" + c);
-                }
+                sb.append(String.valueOf(c).repeat(k));
             }
             
             String s = sb.toString();
