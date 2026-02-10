f3a8ded9d633d580046b597241dc7ad2f4faa196
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-10 14:59:18 +0900

    [level 0] Title: 그림 확대, Time: 10.46 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,23 @@
+class Solution {
+    // picture의 길이의 k배의 배열이 필요
+    // picture를 순회
+    // 요소의 요소를 k개 담은 문자열 생성
+    // 해당 문자열을 k번 넣는다
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        for (int i=0; i<picture.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : picture[i].toCharArray()) {
+                for (int j=0; j<k; j++) {
+                    sb.append("" + c);
+                }
+            }
+            
+            String s = sb.toString();
+            for (int j=0; j<k; j++) {
+                answer[i*k+j] = s;
+            }
+        }
+        return answer;
+    }
+}
