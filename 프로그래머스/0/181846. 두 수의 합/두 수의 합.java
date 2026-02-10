372ccedf032d56f168929b081686d41eb3380c96
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-10 12:43:33 +0900

    [level 0] Title: 두 수의 합, Time: 16.27 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -3,21 +3,18 @@ class Solution {
         int carry = 0;
         int i = a.length()-1, j = b.length()-1;
         StringBuilder sb = new StringBuilder();
-        while (i > -1 || j > -1) {
-            int aNum = 0, bNum = 0;
+        while (i > -1 || j > -1 || carry > 0) {
+            int sum = carry;
+            
             if (i > -1) {
-                aNum = Integer.parseInt((a.charAt(i--) - '0') + "");
+                sum += a.charAt(i--) - '0';
             }
             if (j > -1) {
-                bNum = Integer.parseInt((b.charAt(j--) - '0') + "");
+                sum += b.charAt(j--) - '0';
             }
-            int sum = aNum + bNum + carry;
-            sb.append(String.valueOf(sum % 10));
+            
+            sb.append(sum % 10);
             carry = sum / 10;
-        }
-        
-        if (carry != 0) {
-            sb.append(String.valueOf(carry));
         }
         
         return sb.reverse().toString();
