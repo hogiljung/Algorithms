bb753af877a37535263a842ae19dff75b6d8a64b
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-02-10 11:53:36 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.95 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,20 +1,34 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int answer = 0;
-        for (int i=0; i<3; i++) {
-            int top = 101;
-            int index = 0;
-            for (int j=0; j<rank.length; j++) {
-                if (attendance[j] && rank[j] < top) {
-                    top = rank[j];
-                    index = j;
-                }
+        PriorityQueue<Integer> queue = new PriorityQueue<>(
+            (a, b) -> rank[a] - rank[b]
+        );
+        
+        for (int i=0; i<rank.length; i++) {
+            if (attendance[i]) {
+                queue.add(i);
             }
-            
-            rank[index] = 101;
-            
-            answer += (int) Math.pow(100, 2-i) * index;
         }
+        
+        int answer = 0;
+        int i = 0;
+        while (i < 3) {
+            int n = queue.poll();
+            answer = answer * 100 + n;
+            i++;
+        }
+        
         return answer;
+    }
+    
+    class Attended {
+        int rank;
+        
+        Attended(int rank) {
+            this.rank = rank;
+        }
     }
 }
