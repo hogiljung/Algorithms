2e5ee1e3977a77176b80c697c4fd1ec2b490033e
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-14 15:41:45 +0900

    [level 2] Title: 할인 행사, Time: 28.84 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 110 MB, 시간: 37.96 ms
+메모리: 118 MB, 시간: 28.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 22일 15:43:13
+2026년 07월 14일 15:41:41
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,42 +1,40 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        HashMap<String, Integer> wMap = new HashMap<>();
-        for (int i=0; i<want.length; i++) {
-            wMap.put(want[i], number[i]);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
         }
         
-        HashMap<String, Integer> dMap = new HashMap<>();
-        for (int i=0; i<10; i++) {
-            String d = discount[i];
-            dMap.put(d, dMap.getOrDefault(d, 0) + 1);
+        int count = 0;
+        for (int left = 0, right = 0; right < discount.length; right++) {
+            String d = discount[right];
+            if (map.containsKey(d)) {
+                map.put(d, map.get(d) - 1);
+            }
+            
+            if (right - left + 1 > 10) {
+                d = discount[left++];
+                if (map.containsKey(d)) {
+                    map.put(d, map.get(d) + 1);
+                }
+            }
+            
+            boolean isOk = true;
+            for (int n : map.values()) {
+                if (n > 0) {
+                    isOk = false;
+                    break;
+                }
+            }
+            
+            if (isOk) {
+                count++;
+            }
         }
         
-        int left = 0;
-        int right = 9;
-        int answer = 0;
-        while (right < discount.length) {
-            if (wMap.equals(dMap)) {
-                answer++;
-            }
-            
-            right++;
-            if (right == discount.length) {
-                break;
-            }
-            String d1 = discount[right];
-            dMap.put(d1, dMap.getOrDefault(d1, 0) + 1);
-            
-            String d2 = discount[left];
-            int cnt = dMap.get(d2);
-            if (cnt == 1) {
-                dMap.remove(d2);
-            } else {
-                dMap.put(d2, cnt-1);
-            }
-            left++;
-        }
-        return answer;
+        return count;
     }
 }
