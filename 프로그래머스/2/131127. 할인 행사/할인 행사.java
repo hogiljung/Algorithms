1a7909e15d4ea878a041a0606c826a0a21487fab
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-14 16:14:43 +0900

    [level 2] Title: 할인 행사, Time: 16.27 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,35 +2,45 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> remaining = new HashMap<>();
         
         for (int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+            remaining.put(want[i], number[i]);
         }
         
+        int satisfied = 0;
         int count = 0;
-        for (int left = 0, right = 0; right < discount.length; right++) {
-            String d = discount[right];
-            if (map.containsKey(d)) {
-                map.put(d, map.get(d) - 1);
-            }
+        
+        for (int right = 0; right < discount.length; right++) {
+            String added = discount[right];
             
-            if (right - left + 1 > 10) {
-                d = discount[left++];
-                if (map.containsKey(d)) {
-                    map.put(d, map.get(d) + 1);
+            if (remaining.containsKey(added)) {
+                int next = remaining.get(added) - 1;
+                remaining.put(added, next);
+                
+                if (next == 0) {
+                    satisfied++;
+                } else if (next == -1) {
+                    satisfied--;
                 }
             }
             
-            boolean isOk = true;
-            for (int n : map.values()) {
-                if (n > 0) {
-                    isOk = false;
-                    break;
+            if (right >= 10) {
+                String removed = discount[right - 10];
+                
+                if (remaining.containsKey(removed)) {
+                    int next = remaining.get(removed) + 1;
+                    remaining.put(removed, next);
+                    
+                    if (next == 0) {
+                        satisfied++;
+                    } else if (next == 1) {
+                        satisfied--;
+                    }
                 }
             }
             
-            if (isOk) {
+            if (right >= 9 && satisfied == want.length) {
                 count++;
             }
         }
