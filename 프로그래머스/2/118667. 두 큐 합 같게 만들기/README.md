8cd3db07b9a5d37a150b9969fe1f7fd4d0fdb3ce
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-14 14:28:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 14.66 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 154 MB, 시간: 80.02 ms
+메모리: 107 MB, 시간: 14.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 13일 15:13:48
+2026년 07월 14일 14:28:03
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,43 +1,49 @@
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        Deque<Long> q1 = new LinkedList<>();
-        Deque<Long> q2 = new LinkedList<>();
-        long sum1 = 0, sum2 = 0;
+        int n = queue1.length;
+        int[] q = new int[n * 2];
+    
+        long sumQ1 = 0;
+        long sumQ2 = 0;
         
-        for (int i=0; i<queue1.length; i++) {
-            long q1Item = queue1[i];
-            long q2Item = queue2[i];
-            q1.add(q1Item);
-            q2.add(q2Item);
-            sum1 += q1Item;
-            sum2 += q2Item;
+        for (int i = 0; i < queue1.length; i++) {
+            q[i] = queue1[i];
+            q[i + n] = queue2[i];
+            
+            sumQ1 += queue1[i];
+            sumQ2 += queue2[i];
         }
         
-        int answer = 0;
-        while (sum1 != sum2) {
-            if (sum1 < sum2) {
-                long n = q2.poll();
-                sum2 -= n;
-                q1.add(n);
-                sum1 += n;
-                answer++;
-            } else if (sum1 > sum2) {
-                long n = q1.poll();
-                sum1 -= n;
-                q2.add(n);
-                sum2 += n;
-                answer++;
+        long total = sumQ1 + sumQ2;
+
+        if (total % 2 != 0) {
+            return -1;
+        }
+
+        long target = total / 2;
+
+        int left = 0;
+        int right = n;
+        int count = 0;
+        
+        int maxCount = n * 3;
+        
+        while (count <= maxCount) {
+            if (sumQ1 == target) {
+                return count;
             }
             
-            if (answer >= queue1.length * 4) {
-                answer = -1;
-                break;
+            if (sumQ1 < target) {
+                sumQ1 += q[right % q.length];
+                right++;
+            } else {
+                sumQ1 -= q[left % q.length];
+                left++;
             }
+            
+            count++;
         }
-        return answer;
+        
+        return -1;
     }
 }
