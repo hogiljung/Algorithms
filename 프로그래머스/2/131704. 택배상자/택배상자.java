223a7ce02f08b477ef2274e794bf33b33542f20e
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-23 01:50:06 +0900

    [level 2] Title: 택배상자, Time: 26.07 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,23 +1,30 @@
+/*
+1. 서브 컨테이너가 원하는 상자면 싣는다.
+2. 아니면 메인에서 다음 상자를 보조 벨트로 싣는다.
+3. 더 이상 가져올 상자가 없고 보조 벨트에서도 꺼낼 수 없으면 종료
+*/
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        int count = 0;
+        Deque<Integer> subBelt = new ArrayDeque<>();
+        int nextBox = 1;
+        int loadedCount = 0;
         
-        for (int nextNum = 1; nextNum <= order.length; nextNum++) {
-            if (nextNum == order[count]) {
-                count++;
-            } else {
-                stack.push(nextNum);
-            }
+        while (loadedCount < order.length) {
+            int targetBox = order[loadedCount];
             
-            while (!stack.isEmpty() && stack.peek() == order[count]) {
-                stack.pop();
-                count++;
+            if (!subBelt.isEmpty() && targetBox == subBelt.peek()) {
+                subBelt.pop();
+                loadedCount++;
+            } else if (nextBox <= order.length) {
+                subBelt.push(nextBox++);                
+            } else {
+                break;
             }
         }
         
-        return count;
+        return loadedCount;
     }
 }
