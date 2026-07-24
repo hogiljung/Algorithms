328f81da8f6d206fc6c710ceb35d11256cf0efd0
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-24 23:44:57 +0900

    [level 3] Title: 부대복귀, Time: 120.01 ms, Memory: 214 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -0,0 +1,58 @@
+/*
+s->d로 가는 길을 찾으며 중간 경로를 기록
+s = n
+a = n-1
+...
+b = 1
+다음 길을 찾으며 만나면 바로 계산 완료
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        List<Integer>[] map = new ArrayList[n];
+        int[] distance = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
+        
+        Arrays.fill(distance, -1);
+        distance[destination-1] = 0;
+        
+        for (int[] road : roads) {
+            map[road[0] - 1].add(road[1] - 1);
+            map[road[1] - 1].add(road[0] - 1);
+        }
+        
+        bfs(map, distance, destination - 1);
+        
+        int[] answer = new int[sources.length];
+        
+        for (int i = 0; i < sources.length; i++) {
+            int source = sources[i];
+            answer[i] = distance[source - 1];
+        }
+        
+        return answer;
+    }
+    
+    private void bfs(List<Integer>[] map, int[] distance, int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        distance[start] = 0;
+        queue.offer(start);
+        
+        while (!queue.isEmpty()) {
+            int s = queue.poll();
+            
+            for (int d : map[s]) {
+                if (distance[d] != -1)
+                    continue;
+                
+                distance[d] = distance[s] + 1;
+                queue.offer(d);
+            }
+        }
+    }
+}
