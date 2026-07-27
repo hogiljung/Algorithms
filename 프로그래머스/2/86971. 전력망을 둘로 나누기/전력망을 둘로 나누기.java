6ecf98eee0b0e975a0b95136c1a59767250c3139
hogiljung <112647829+hogiljung@users.noreply.github.com>
2026-07-27 23:51:58 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.26 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,58 @@
 import java.util.*;
 
 class Solution {
+    int[] parent;
+
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        
-        for (int i = 0; i < wires.length; i++) {
-            List<Integer>[] graph = new ArrayList[n+1];
-            
-            for (int j = 0; j <= n; j++) {
-                graph[j] = new ArrayList<>();
+
+        for (int removed = 0; removed < wires.length; removed++) {
+            parent = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
             }
-            
-            for (int j = 0; j < wires.length; j++) {
-                if (i == j) continue;
-                
-                int[] wire = wires[j];
-                graph[wire[0]].add(wire[1]);
-                graph[wire[1]].add(wire[0]);
+
+            // removed번째 전선만 제외하고 합치기
+            for (int i = 0; i < wires.length; i++) {
+                if (i == removed) {
+                    continue;
+                }
+
+                union(wires[i][0], wires[i][1]);
             }
-            
-            boolean[] visited = new boolean[n+1];
-            
+
+            // 1번 노드가 속한 컴포넌트 크기 계산
+            int root = findParent(1);
             int count = 0;
-            
-            for (int j = 1; j <= n; j++) {
-                if (visited[j]) continue;
-                
-                count = dfs(graph, visited, j);
+
+            for (int i = 1; i <= n; i++) {
+                if (findParent(i) == root) {
+                    count++;
+                }
             }
-            
-            answer = Math.min(answer, Math.abs(count - (n - count)));
+
+            int otherCount = n - count;
+            answer = Math.min(answer, Math.abs(count - otherCount));
         }
 
         return answer;
     }
-    
-    private int dfs(List<Integer>[] graph, boolean[] visited, int start) {
-        visited[start] = true;
-        int count = 1;
-        
-        for (int node: graph[start]) {
-            if (visited[node]) continue;
-            
-            count += dfs(graph, visited, node);
+
+    int findParent(int x) {
+        if (parent[x] == x) {
+            return x;
         }
-        
-        return count;
+
+        return parent[x] = findParent(parent[x]);
+    }
+
+    void union(int a, int b) {
+        int rootA = findParent(a);
+        int rootB = findParent(b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
     }
 }
